137e23bd9e0c60b7ed7b565e9c006717bd7c7ef1
Won0721 <133324320+Won0721@users.noreply.github.com>
2023-06-01 13:37:11 +0900

    산술연산자에서 정수타입 예제

--- java_git/src/day2/operator/ArithmeticEx.java
+++ java_git/src/day2/operator/ArithmeticEx.java
@@ -10,8 +10,12 @@ public class ArithmeticEx {
 		int mod = num1 % num2;
 		
 		
-		System.out.println("" + num1 + "+" + num2 + "=" + sum);
-		System.out.println("" + num1 + "-" + num2 + "=" + sub);
+		// 정수 뒤에 char 형 문자가 오게 될 경우 문자가 정수로 표현되어 String으로 바꾸거나 정수형 앞에 "" 입력
+		System.out.println(""+(num1+num2));
+		System.out.println(""+num1+num2);
+		
+		System.out.println(num1 + "+" + num2 + "=" + sum);
+		System.out.println(num1 + "-" + num2 + "=" + sub);
 		System.out.println("" + num1 + "*" + num2 + "=" + mul);
 		System.out.println("" + num1 + "/" + num2 + "=" + div);
 		System.out.println("" + num1 + "%" + num2 + "=" + mod);
@@ -20,6 +24,7 @@ public class ArithmeticEx {
 		// 1 + res = 2; // 대입연산자 = 왼쪽에 식이 와서 에러가 발생
 		res = 2 - 1; 	// 대입연산자 오른쪽에 식이와서 에러없이 실행됨
 		// 1 = res + 1; // 왼쪽에는 리터럴이 올 수 없음
+		
 		
 		
 	
