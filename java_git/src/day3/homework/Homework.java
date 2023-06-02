f048e2eab50f49a0759ab236ddc6c8254eb397be
Won0721 <133324320+Won0721@users.noreply.github.com>
2023-06-02 12:45:44 +0900

    day3 과제

--- java_git/src/day3/homework/Homework.java
+++ java_git/src/day3/homework/Homework.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Homework {
 	public static void main(String[] args) {
-		/* 성별을 입력받아 여성인지 확인하는 코드를 작성하세요. 
+		/* 성별(M:남성, W:여성)을 입력받아 여성인지 확인하는 코드를 작성하세요. 
 		 * 1. 조건연산자 사용
 		 * 
 		 * input gender : 
@@ -18,23 +18,27 @@ public class Homework {
 		 */
 		Scanner sc = new Scanner(System.in);
 		
+		// 1. 삼항연산자 사용
 		
 		System.out.println("성별을 입력해주세요 (M or W)");
 		String result = sc.next();
-		System.out.println("여성입니까? " + (result == "Man" ? true : false));
+		System.out.println("여성입니까? " + (result.equals("W") ? true : false));
 		
-	
-		
+
 		
 		// 2. if문 사용
 		System.out.println("성별을 입력해주세요 (Man or Woman)");
 		String gender;
+		String sex1 = "Man";
+		String sex2 = "Woman";
+		
 		gender = sc.next();
-		if(gender.equals("Man")) {
-			System.out.println("여성입니까? "+ gender);
+		
+		if(gender.equals("M")) {
+			System.out.println("여성입니까? "+ sex1);
 		}
-		else if(gender.equals("Woman")){
-			System.out.println("여성입니까? "+ gender);
+		else if(gender.equals("W")){
+			System.out.println("여성입니까? "+ sex2);
 		}
 	
 		
