a08ed3a375c5b1bb9fa4157ab36227e4eb9e0936
Won0721 <133324320+Won0721@users.noreply.github.com>
2023-06-02 12:45:34 +0900

    day3 과제

---
+++ java_git/src/day3/homework/HomeEx.java
@@ -0,0 +1,64 @@
+package day3.homework;
+
+import java.util.Scanner;
+
+public class HomeEx {
+	public static void main(String[] args) {
+		/*
+		 * 성별(M:남성, W:여성)을 입력받아 여성인지 확인하는 코드를 작성하세요. 
+		 * 1. 조건연산자 사용
+		 * 
+		 * input gender : 
+		 * M
+		 * Are you a women? false
+		 */
+//		char gender;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("input gender :");
+//		gender = sc.next().charAt(0);
+//		boolean result = gender == 'W' ? true: false;
+//		System.out.println("Are you a women? " + result);
+		
+//		String gender;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("input gender :");
+//		gender = sc.next();
+//		boolean result = gender.equals("W") ? true: false;
+//		System.out.println("Are you a women? " + result);
+		
+		 /* 2. if 문 사용
+		 * input gender :
+		 * M
+		 * are you a women? man
+		 */
+		
+//		char gender; 
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("input gender : ");
+//		gender = sc.next().charAt(0);
+//		if(gender == 'M') {
+//			System.out.println("Are you a women? " + "man");	
+//		}
+//		else if(gender =='W') {
+//			System.out.println("Are you a women? " + "women");
+//		}
+		
+		String gender; 
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input gender : ");
+		gender = sc.next();
+		String result = "";
+		if(gender.equals("M")) {
+			result = "man";
+		}
+		else if(gender.equals("W")) {
+			result = "women";
+		}
+		System.out.println("Are you a women? " + result);
+		
+		
+		
+		sc.close();
+	}
+
+}
