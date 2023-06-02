87cb049728e0926390e0dfe49240f0f495568557
Won0721 <133324320+Won0721@users.noreply.github.com>
2023-06-02 12:46:11 +0900

    if문 양수음수

---
+++ java_git/src/day4/ifEx/NumberEx.java
@@ -0,0 +1,29 @@
+package day4.ifEx;
+
+import java.util.Scanner;
+
+public class NumberEx {
+	public static void main(String[] args) {
+		/* 정수 num가 양수이면 positive number라고 출력하고
+		 * 음수이면 negative number 라고 출력
+		 * 0이면 0으로 출력하는 코드를 작성하세요
+		 */
+		int num;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input number : ");
+		num = sc.nextInt();
+		String result ="";
+		if(num>0) {
+			result ="positive number";
+			
+		}
+		else if(num<0) {
+			result = "negative number";
+		}	
+		else {
+			result = "zero";
+		}
+		System.out.println(num + " is " + result);
+	}
+
+}
