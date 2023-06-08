75241020f5e360ffd6de85440f86e64a9dc368b9
Won0721 <133324320+Won0721@users.noreply.github.com>
2023-06-08 14:43:57 +0900

    for 문 별찍기

---
+++ java_git/src/day7/practice/StarEx3.java
@@ -0,0 +1,23 @@
+package day7.practice;
+
+public class StarEx3 {
+	public static void main(String[] args) {
+		
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<4-i; j++) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<i+1; j++) {
+				System.out.print("*");
+			}
+			System.out.print(" ");
+			for(int j=0; j<i+1; j++) {
+				System.out.print("*");
+			}
+			
+			
+			System.out.println();
+		}
+	}
+
+}
