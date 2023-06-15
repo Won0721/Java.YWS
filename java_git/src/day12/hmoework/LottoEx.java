cd653ba00d22d06780b3234cdee9f47aff44e3da
Won0721 <133324320+Won0721@users.noreply.github.com>
2023-06-15 14:48:07 +0900

    로또 숙제

---
+++ java_git/src/day12/hmoework/LottoEx.java
@@ -0,0 +1,22 @@
+package day12.hmoework;
+
+public class LottoEx {
+	/* 1등 : 번호: 6개
+	 * 2등 : 번호 5개 + 보너스
+	 * 3등 : 번호 5개
+	 * 4등 : 번호 4개
+	 * 5등 : 번호 3개
+	 * .. : X
+	 * 
+	 * com :
+	 * 	number : [1, 3, 40, 23, 24, 45]
+	 * 	bonus : 2
+	 * 
+	 * user :
+	 * 	 1 2 3 4 5 6 
+	 * 
+	 * losing ticket!
+	 * 
+	 */
+
+}
