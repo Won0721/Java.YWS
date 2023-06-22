0b6768c8c1f9e25731d0878d2f8c323175910650
Won0721 <133324320+Won0721@users.noreply.github.com>
2023-06-22 13:56:36 +0900

    2
    2

--- java_git/src/friendmanager/Friend.java
+++ java_git/src/friendmanager/Friend.java
@@ -1,6 +1,17 @@
 package friendmanager;
 
-public class Friend {
-		public void showBasicInfo() {}
+public abstract class Friend {
+	private String name;
+	private String phoneNUm;
+	private String addr;
+	
+		public void showDate() {
+			System.out.println("이름 : "  + name);
+			System.out.println("폰번호 : "  + phoneNUm);
+			System.out.println("주소 : "  + addr);
+		}
+	
+	
+		public abstract void showBasicInfo();
 
 }
