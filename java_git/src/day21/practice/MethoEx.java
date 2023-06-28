9ee6f84a429717faedf61aadb3d2ab0a63c8fbd3
Won0721 <133324320+Won0721@users.noreply.github.com>
2023-06-28 15:16:58 +0900

    1

---
+++ java_git/src/day21/practice/MethoEx.java
@@ -0,0 +1,26 @@
+package day21.practice;
+
+public class MethoEx {
+	public static void main(String[] args) {
+		System.out.println(sum(1));
+		System.out.println(sum(1,2));
+		System.out.println(sum(1,2,3,4,5,6));
+		System.out.println(sum(1,10));
+		
+		int a = Math.max(4, 2);
+		
+		System.out.println(a);
+	}
+	
+	
+	/* 여러개의 정수의 합을 알려주는 메서드 */
+	
+	public static int sum(int ... nums) {
+		int sum = 0;
+		for(int i=0; i < nums.length; i++) {
+			sum += nums[i];
+		}
+		return sum;
+	}
+	
+}
