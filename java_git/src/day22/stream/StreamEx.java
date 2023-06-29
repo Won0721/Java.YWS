ff1248c73dcf0d6bddc9306c2b7ff57dddf47ad1
Won0721 <133324320+Won0721@users.noreply.github.com>
2023-06-29 14:01:42 +0900

    Stream 예제

---
+++ java_git/src/day22/stream/StreamEx.java
@@ -0,0 +1,41 @@
+package day22.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamEx {
+	public static void main(String[] args) {
+		// 스트림 사용 이유 : 방법이 같아짐
+		Integer[] arr = {1,2,3,4,5};
+		
+		List<Integer> list = Arrays.asList(1,2,3,4,5);
+		Stream<Integer> arrStream = Arrays.stream(arr);
+		Stream<Integer> listStream = list.stream();
+		
+		// 전체출력 
+		// 매개변수 ㅇ 리턴타입 없음
+		arrStream.forEach(num -> System.out.print(num + " "));
+		System.out.println();
+		listStream.forEach(num -> System.out.print(num + " "));
+		System.out.println();
+		
+		System.out.println("==필터사용==");
+		
+		// 3 보다 큰 수 출력
+		// 매개변수 o 리턴타입 boolean
+		// 스트림은 한번 사용하면 사라지기에 다시 만들어줘야함
+		arrStream = Arrays.stream(arr);
+		arrStream
+				.filter(num -> num>3)
+				.forEach(num -> System.out.println(num));
+		
+		listStream = list.stream();
+		System.out.println("");
+		listStream
+		.filter(num -> num>3)
+		.forEach(num -> System.out.println(num));
+		
+	}
+
+}
