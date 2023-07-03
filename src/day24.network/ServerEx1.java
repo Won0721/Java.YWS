45435f726cb25fa3639d9a1e2f1ec8d9cdcdfdc1
Won0721 <133324320+Won0721@users.noreply.github.com>
2023-07-03 13:13:06 +0900

    1

---
+++ src/day24.network/ServerEx1.java
@@ -0,0 +1,35 @@
+package day24.network;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ServerEx1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ServerSocket serverSocket = new ServerSocket();
+		int port = 5001;
+		serverSocket = new SeverSocket(port);
+		while(true) {
+			System.out.println("waiting");
+			
+			Socket socket = serverSocket.accept();
+			System.out.println("connect");
+			
+			InputStream is = socket.getInputStream();
+			byte [] bytes = new byte[1024];
+			int readCount = is.read();
+			String str = new String(bytes);
+			System.out.println("data : " + str + " ]");
+			
+			// 클라이언트로 데이터를 보내기 위해 OutputStream 생성
+			OutputStream os = socket.getOutputStream();
+			str = sc.nextLine();
+			
+		}
+		
+	}
+
+}
